5d7a046c9605e1cb3f49d27ee374576699212093
AcoStar <greenshiftkaito@hotmail.com>
2021-12-02 17:56:17 +0300

    Перенос JS, isset в php

--- index.php
+++ index.php
@@ -2,32 +2,17 @@
     <head>
         <link rel="stylesheet" href="style.css">
         <title>Dev</title>
-        <script>
-            // Включение/Выключение кнопки отправки
-            function UpdateSubmitButton() {
-                let name = document.getElementById("name");
-                let age = document.getElementById("age");
-                let submit = document.getElementById("submit");
-                if (name.value == "" || age.value == "")
-                {
-                    submit.disabled = true;
-                }
-                else
-                {
-                    submit.disabled = false;
-                }
-            }
-        </script>
+		<script src="script.js"></script>
     </head>
     <body>
         <div class="line">
             <div class="panel">
                 <h1>Отправка данных</h1>
-                <form action="index.php" method="get" target="_self">
+                <form action="index.php" method="get" target="_self" autocomplete="off">
                     <label for="name">Имя</label>
-                    <input type="text" id="name" name="name" onChange="UpdateSubmitButton();">
+                    <input type="text" id="name" name="name" required>
                     <label for="age">Возраст</label>
-                    <input type="number" id="age" name="age" min="10" max="100" onChange="UpdateSubmitButton();">
+                    <input type="number" id="age" name="age" min="10" max="100" required>
                     <input type="submit" id="submit" value="Отправить на сервер" disabled>
                 </form>
             </div>
@@ -35,8 +20,11 @@
                 <h1>Ответ сервера</h1>
                 <p class="response">
                     <?php
-                        $name = htmlspecialchars($_GET["name"]);
-                        $age = htmlspecialchars($_GET["age"]);
+                        if (isset($_GET["name"]))
+                            $name = htmlspecialchars($_GET["name"]);
+                        if (isset($_GET["age"]))
+                            $age = htmlspecialchars($_GET["age"]);
+
                         //  Если есть имя - отображаем его
                         if ($name && $name != "")
                             echo "<strong>" . $name . "</strong><br>Длина имени: " . mb_strlen($name, 'UTF-8') . "<br>";
