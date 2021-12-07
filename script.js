9d7ca6c3edd305b81ce24a1b122546dca39f03be
AcoStar <greenshiftkaito@hotmail.com>
2021-12-07 07:16:28 +0300

    Учитываем дату

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let username;
 let age;
+let date;
 let submit;
 function UpdateSubmitButton() {
-    if (username.value == "" || age.value == "")
+    if (username.value == "" || age.value == "" || date.value == "")
     {
         submit.disabled = true;
     }
@@ -16,7 +17,9 @@ window.onload = function() {
     username = document.getElementById("name");
     age = document.getElementById("age");
     submit = document.getElementById("submit");
-    
+    date = document.getElementById("date");
+
     username.addEventListener("change", UpdateSubmitButton);
     age.addEventListener("change", UpdateSubmitButton);
+    date.addEventListener("change", UpdateSubmitButton);
 }
