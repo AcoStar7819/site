02692163e3a7c224ddb600624eb1bf04418deb6b
AcoStar <greenshiftkaito@hotmail.com>
2021-12-07 05:57:02 +0300

    Сократил кол-во getElementById

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
+
 function UpdateSubmitButton() {
-    let name = document.getElementById("name");
-    let age = document.getElementById("age");
-    let submit = document.getElementById("submit");
     if (name.value == "" || age.value == "")
     {
         submit.disabled = true;
@@ -13,6 +11,9 @@ function UpdateSubmitButton() {
 }
 
 window.onload = function() {
-    document.getElementById("name").addEventListener("change", UpdateSubmitButton);
-    document.getElementById("age").addEventListener("change", UpdateSubmitButton);
+    var name = document.getElementById("name");
+    var age = document.getElementById("age");
+    var submit = document.getElementById("submit");
+    name.addEventListener("change", UpdateSubmitButton);
+    age.addEventListener("change", UpdateSubmitButton);
 }
